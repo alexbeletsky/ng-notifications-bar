8d10ccf961e4384398782eb895cd24894881bb19
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-09-18 11:08:25 +0300

    boilerplate

---
+++ src/ng-notifications-bar.js
@@ -0,0 +1,14 @@
+(function (window, angular) {
+	var module = angular.module('ngNotificationsBar', []);
+
+	module.provider('ngNotificationsBar', function () {
+		this.$get = function () {
+
+		};
+	});
+
+	module.directive('ngNotificationsBar', function () {
+
+	});
+
+})(window, angular);
